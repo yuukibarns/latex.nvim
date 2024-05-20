9f13116e25ffa68e83a821fd65669c35801865a1
Jia Jia <mathjiajia@outlook.com>
2024-05-20 21:05:51 +0800

    fix: 0.11-dev

--- lua/latex/module/texlab.lua
+++ lua/latex/module/texlab.lua
@@ -1,11 +1,11 @@
 local api, lsp = vim.api, vim.lsp
 
-local texlab_build_status = vim.tbl_add_reverse_lookup({
-	Success = 0,
-	Error = 1,
-	Failure = 2,
-	Cancelled = 3,
-})
+local texlab_build_status = {
+	[0] = "Success",
+	[1] = "Error",
+	[2] = "Failure",
+	[3] = "Cancelled",
+}
 
 local M = {}
 
